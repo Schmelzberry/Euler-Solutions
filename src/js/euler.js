c2d7fb423b40cd2ae7b5d45742420bab47c9b2ba
Jake Elsberry <jelsberry96@gmail.com>
2023-11-08 11:25:43 -0800

    problem #1 solution achieved recursively

--- src/js/euler.js
+++ src/js/euler.js
@@ -1,21 +1,24 @@
-// Problem #1 Euler Archives
+// Problem #1 - Euler Archives
 
+// QUESTION: 
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
+// ANSWER: 
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
 const multipleCount = (num) => {
-  if (isNaN(num)) {
-    return "please enter a number";
-  }
-
-  else if (num > 30) {
+  // base case, count until 99
+  if (num >= 100) {
     return;
   }
-  else {
+  // check if a number is divisible by 3 or 5, log to console if true
+  if (num % 3 === 0 || num % 5 === 0) {
     console.log(num);
-    return multipleCount(num + 1)
   }
-}
-
+  // recursive call, increment number
+  multipleCount(num + 1);
+};
+// call function so webpack can stfu
 multipleCount(20);
+
+// Problem #1 - Euler Archives
